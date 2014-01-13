97b5841ccd605d17063e8e865e8c98801d175c68
Brianna Shade <jarvis.brianna@gmail.com>
2014-01-12 17:09:07 -0800

    Initializing project Machine Learning

---
+++ Perceptrons.java
@@ -0,0 +1,17 @@
+/**
+ * "Perceptrons"
+ * CS545: Machine Learning - HW1
+ * Copyright © 2014 Brianna Shade
+ * bshade@pdx.edu
+ *
+ * Perceptrons.java
+ * TODO details on this class
+ */
+package hw1Perceptrons;
+
+public class Perceptrons {
+
+	public static void main(String args[]){
+		
+	}
+}
