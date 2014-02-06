fdd21d423e59066ec85dc8860c9082a1334f3805
Brianna Shade <jarvis.brianna@gmail.com>
2014-02-05 22:43:39 -0800

    Added random number and binary search for roulette wheel

--- hw2SVM/boost.pl
+++ hw2SVM/boost.pl
@@ -2,7 +2,6 @@
 
 use strict;
 use warnings;
-package hw2SVMs;
 
 # "Boost" Adaboost algorithm script
 # © 2014 Brianna Shade
@@ -11,13 +10,25 @@ package hw2SVMs;
 my @w;
 
 sub updateWeights{
-    my ($incr) = @_;
-    for(my $i = 0; $i < @w; $i++){
+    my ($wsize, $incr) = @_;
+    for(my $i = 0; $i < $wsize; $i++){
         my $prvbndry;
         if ($i == 0) { $prvbndry = 0 }
         else { $prvbndry = $w[$i-1] }
         $w[$i] = $prvbndry + $incr;
     }
+}
+
+sub locateInstance{
+    my($min, $max, $num) = @_;
+    if($min == $max) { return $max }
+    if($max - $min < 2){
+        if($num > $min) { return $min }
+        else { return $max}
+    }
+    my $mid = int((($max - $min) / 2) + $min);
+    if($num <= $w[$mid]) { return locateInstance($min, $mid, $num) }
+    if($num > $w[$mid]) { return locateInstance($mid, $max, $num) }
 }
 
 sub run{
@@ -37,24 +48,26 @@ sub run{
 
     # Initialize weight vector
     my $M = @instances;
-    @w = (1) x $M;
-    updateWeights(1/$M);
-    print join(" ", @w) . "\n";
-    print $M . "\n";
+    updateWeights($M, 1/$M);
+#    print join(" ", @w) . "\n";
 
-    #foreach my $T (0..$K-1){                                    #boosting iterations
-    #    my $train = "S$T";
-    #    my $hypoth = "h$T";
-    #    my $predict = "predict/$T.predictions";
-    #    my $testpred = "predict/Test$T.predictions";
+#    foreach my $T (0..$K-1){                                    #boosting iterations
+#        my $train = "S$T";
+#        my $hypoth = "h$T";
+#        my $predict = "predict/$T.predictions";
+#        my $testpred = "predict/Test$T.predictions";
 
-    #    my @trainex;
-    #    foreach my $i (0..$M-1){
+        my @trainex;
+#        foreach my $i (0..$M-1){
         #select training example based on roulette wheel
+        my $r = rand();
+        print "r: $r";
+        my $exnum = locateInstance(0, $M-1, $r);
+        print ": $exnum ($w[$exnum-1]-$w[$exnum])\n";
     #        open(FILE, ">S$i");
     #        print FILE join(" ", @trainex);
     #        close(FILE);
-    #    }
+#        }
 
     # SVM
     #    system("svm_learn $kernel $train $model");          #train
@@ -68,7 +81,7 @@ sub run{
     #    system("svm_classify $test $hypoth $testpred");
         #extract sign of first row in $testpred (+ or -)
         #compute H(x) for every x in the test set sum(alpha$T * $hypoth)
-    #}
+#    }
 }
 
 run();
