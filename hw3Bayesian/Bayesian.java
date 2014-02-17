62ad17e7257b128d99ef5e32c895edcab66e05cb
Brianna Shade <jarvis.brianna@gmail.com>
2014-02-17 07:44:51 -0800

    Preliminary code layout

---
+++ hw3Bayesian/Bayesian.java
@@ -0,0 +1,26 @@
+/**
+ * "Bayesian Network Learning"
+ * Copyright © 2014 Brianna Shade
+ * bshade@pdx.edu
+ *
+ * Bayesian.java
+ * TODO details on this class
+ */
+package hw3Bayesian;
+
+public class Bayesian {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO: Read in data file
+		// TODO: Compute prior probability (included in optdigits.info)
+		// TODO: Compute conditional probabilities for each digit, for each attribute value
+		// TODO: Smooth conditional probabilities
+		// TODO: Run naive Bayes on training data
+		// TODO: Calculate accuracy of test data and single confusion matrix for all 10 digits
+		// TODO: 4 bins
+	}
+
+}
