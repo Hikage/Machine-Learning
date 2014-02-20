7044304c3687c6896640654f5efc56a044917fe0
Brianna Shade <jarvis.brianna@gmail.com>
2014-02-19 21:18:26 -0800

    Preliminary code for naive Bayes classification

--- hw3Bayesian/Bayesian.java
+++ hw3Bayesian/Bayesian.java
@@ -17,27 +17,17 @@ import java.util.Arrays;
 public class Bayesian {
 
 	private static ArrayList<int[]> trainData = new ArrayList<int[]>();
+	private static ArrayList<int[]> testData = new ArrayList<int[]>();
 	private static int[] trainCt = {376, 389, 380, 389, 387, 376, 377, 387, 380, 382};
 	private static int trainTtl = 3823;
 	private static double[] trainProb = new double[trainCt.length];
 	private static double[][][] CPtrain = new double[64][17][10];
-	
-	private static double[] Ptest = {
-		178/1797,		//0
-		182/1797,		//1
-		177/1797,		//2
-		183/1797,		//3
-		181/1797,		//4
-		182/1797,		//5
-		181/1797,		//6
-		179/1797,		//7
-		174/1797,		//8
-		180/1797};		//9
+	private static double[] testProb = {178.0, 182.0, 177.0, 183.0, 181.0, 182.0, 181.0, 179.0, 174.0, 180.0};
 	
 	/**
 	 * Reads input file and generates training data set
 	 */
-	public static void extractData(String inputFile){		
+	public static void extractData(String inputFile, boolean train){		
 		FileReader fr;
 		BufferedReader buff;
 		
@@ -53,7 +43,10 @@ public class Bayesian {
 				for(int i = 0; i < feats.length; i++){
 					instance[i] = Integer.parseInt(feats[i]);
 				}
-				trainData.add(instance);					//add to training or test
+				
+				//add to training or test
+				if(train) trainData.add(instance);
+				else testData.add(instance);
 			}
 		}
 		catch(IOException ex){
@@ -74,25 +67,74 @@ public class Bayesian {
 		//Laplace smoothing
 		for(double[][] digitvals : CPtrain){
 			for(double[] digits : digitvals){
-				Arrays.fill(digits, 1.0);							//initialize entire array to 1 so all counts are 1 greater
+				Arrays.fill(digits, 1.0);									//initialize entire array to 1 so all counts are 1 greater
 			}
 		}
-		for(int i = 0; i < trainCt.length; i++) trainCt[i] += 17;	//17 different values each attribute could take
+		for(int i = 0; i < trainCt.length; i++) trainCt[i] += 17;			//17 different values each attribute could take
 		
 		for(int[] instance : trainData){
 			for(int i = 0; i < instance.length-1; i++){
 				CPtrain[i][instance[i]][instance[instance.length-1]]++;		//increment count of [feature number][feature value][class]
-				//if(i == 0) System.out.println("["+ i + "][" + instance[i] + "][" + instance[instance.length-1] + "]");
 			}
 		}
 		for(int i = 0; i < CPtrain.length; i++){
 			for(int j = 0; j < CPtrain[0].length; j++){
 				for(int k = 0; k < CPtrain[0][0].length; k++){
-					//System.out.println(CPtrain[i][j][k] + "/" + trainProb[k] + " = " + CPtrain[i][j][k] / trainProb[k]);
-					//if(i == 0) System.out.println(j + ", " + k + ": " + CPtrain[i][j][k] + "/" + trainCt[k]);
 					CPtrain[i][j][k] /= trainCt[k];
 				}
 			}
+		}
+	}
+	
+	public static int classifyInst(int inst){
+		int[] instance = testData.get(inst);
+		
+		if(testProb[0] > 1){
+			System.err.println("testProb hasn't yet been initialized fully with probabilities!");
+			System.exit(0);
+		}
+		
+		double[] classProbs = testProb.clone();
+		
+		//iterate through each possible classification (0-9) and through each feature
+		for(int i = 0; i < classProbs.length; i++){
+			for(int j = 0; j < instance.length-1; j++){
+				classProbs[i] *= CPtrain[j][instance[j]][i];	//multiply in probability att j has value of instance[j] for class i
+			}
+		}
+		
+		//find classification with highest probability
+		double max = 0;
+		int clsif = -1;
+		for(int i = 0; i < classProbs.length; i++){
+			classProbs[i] = Math.log(classProbs[i]);
+			if(classProbs[i] > max){
+				max = classProbs[i];
+				clsif = i;
+			}
+		}
+		
+		return clsif;
+	}
+	
+	public static void naiveBayesClass(String testFile){
+		for(int i = 0; i < testProb.length; i++) testProb[i] /= 1797;
+		
+		extractData(testFile, false);
+		
+		int[] correct = new int[testProb.length];
+		for(int i = 0; i < testData.size(); i++){
+			int clsif = classifyInst(i);
+			if(clsif < 0){
+				System.err.println("Error finding maximum probability for classification");
+				System.exit(0);
+			}
+			
+			if(clsif == testData.get(i)[testData.get(i).length-1]) correct[clsif]++;
+		}
+		
+		for(int i = 0; i < correct.length; i++){
+			System.out.println(i + ": " + correct[i]);
 		}
 	}
 	
@@ -100,12 +142,11 @@ public class Bayesian {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		if(!runTests(true, args[0])) System.exit(0);
+		if(!runTests(true, args[0], args[1])) System.exit(0);
 		
-		//extractData(args[0]);
+		//extractData(args[0], true);
 		//calcProbs();
 		//calcCondProbs();
-		// TODO: Smooth conditional probabilities
 		// TODO: Run naive Bayes on training data
 		// TODO: Calculate accuracy of test data and single confusion matrix for all 10 digits
 		// TODO: 4 bins
@@ -185,11 +226,12 @@ public class Bayesian {
 		return true;
 	}
 	
-	public static boolean runTests(boolean verbose, String trainFile){
+	public static boolean runTests(boolean verbose, String trainFile, String testFile){
 		clearData();
-		extractData(trainFile);
+		extractData(trainFile, true);
 		if(!testCalcProbs()) return false;
 		if(!testCalcCondProbs()) return false;
+		naiveBayesClass(testFile);
 		return true;
 	}
 
