85a466a5537d1b03117f00a805cff4d0daa7ce07
Brianna Shade <jarvis.brianna@gmail.com>
2014-03-02 16:56:13 -0800

    Initializing homework 4 - K-means clustering

---
+++ hw4Kmeans/Kmeans.java
@@ -0,0 +1,21 @@
+/**
+ * "K-means Clustering"
+ * Copyright © 2014 Brianna Shade
+ * bshade@pdx.edu
+ *
+ * Kmeans.java
+ * TODO details on this class
+ */
+package hw4Kmeans;
+
+public class Kmeans {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
